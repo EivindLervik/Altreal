181f3c1dbf60febbe481b2523cf69fb7febec246
EivindLervik <eivind_fantaboy@msn.com>
2018-04-15 15:55:13 +0200

    Added some dao-objects

---
+++ Assets/Scripts/Network/EntityObjects/E_Room.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class E_Room : E_Object {
+
+    public E_Room(JSONNode dbObject)
+    {
+        base.SetupObject(dbObject["transform"]);
+
+
+    }
+
+}
