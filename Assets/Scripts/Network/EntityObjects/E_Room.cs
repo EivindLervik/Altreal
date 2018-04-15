870b812affe34ed1c3c4e31432d9a7013b776a9c
EivindLervik <eivind_fantaboy@msn.com>
2018-04-15 16:41:02 +0200

    Added room-item support

--- Assets/Scripts/Network/EntityObjects/E_Room.cs
+++ Assets/Scripts/Network/EntityObjects/E_Room.cs
@@ -5,11 +5,13 @@ using SimpleJSON;
 
 public class E_Room : E_Object {
 
+    protected List<E_Object> roomObjects;
+
     public E_Room(JSONNode dbObject)
     {
         base.SetupObject(dbObject["transform"]);
 
-
+        roomObjects = new List<E_Object> (dbObject["objects"].AsArray);
     }
 
 }
---
+++ Assets/Scripts/Network/EntityObjects/E_RoomItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class E_RoomItem : E_Object {
+
+    public string itemType;
+    public string styleName;
+
+    public E_RoomItem (JSONNode dbObject)
+    {
+        base.SetupObject(dbObject["transform"]);
+
+        itemType = dbObject["itemType"];
+        styleName = dbObject["styleName"];
+    }
+}
