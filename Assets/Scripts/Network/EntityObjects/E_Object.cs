417049493ac9e48514ec96cd147e53bfeb9acfda
EivindLervik <eivind_fantaboy@msn.com>
2018-04-15 15:55:13 +0200

    Added some dao-objects

---
+++ Assets/Scripts/Network/EntityObjects/E_Object.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class E_Object {
+
+    // Transforms
+    protected Vector3 position;
+    protected Vector3 rotation;
+    protected Vector3 scale;
+
+    // Setup
+    protected void SetupObject(JSONNode transform)
+    {
+        position = new Vector3(transform["position"][0].AsFloat, transform["position"][1].AsFloat, transform["position"][2].AsFloat);
+        rotation = new Vector3(transform["rotation"][0].AsFloat, transform["rotation"][1].AsFloat, transform["rotation"][2].AsFloat);
+        scale = new Vector3(transform["scale"][0].AsFloat, transform["scale"][1].AsFloat, transform["scale"][2].AsFloat);
+    }
+
+}
---
+++ Assets/Scripts/Network/EntityObjects/E_Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class E_Player : E_Object {
+
+    public E_Player(JSONNode dbObject)
+    {
+        base.SetupObject(dbObject["transform"]);
+
+
+    }
+
+}
