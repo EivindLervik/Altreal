6766e3d430bac314be2b2dd216bfe0913442bf71
EivindLervik <eivind.lervik@outlook.com>
2019-01-22 21:42:26 +0100

    Added more to the crazy-taxi game.

---
+++ Assets/MainCarScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCarScript : MonoBehaviour
+{
+
+    public DrivingGameController dgc;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EnemyCarScript ecs = other.GetComponent<EnemyCarScript>();
+        if (ecs && dgc)
+        {
+            dgc.Crash();
+        }
+    }
+
+    public void CrashFinished()
+    {
+        dgc.ShowLeaderboards();
+    }
+}
--- Assets/Scripts/Minigames/DrivingGameController.cs
+++ Assets/Scripts/Minigames/DrivingGameController.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrivingGameController : MonoBehaviour
 {
     [Header("Score")]
     public bool gameOn;
     public int level = 1;
+    public int score = 0;
+    private float actualScore;
+    public Text level_text;
+    public Text score_text;
+    public RectTransform leaderboard_object;
+    public RectTransform score_object;
+    public RectTransform level_object;
+
+    [Header("Difficulty")]
+    public float difficulty;
+    public float scoreModifier;
 
     [Header("Roads")]
     public Transform[] roads;
@@ -17,6 +29,7 @@ public class DrivingGameController : MonoBehaviour
 
     [Header("Cars")]
     public Transform playerCar;
+    public MainCarScript playerCarScript;
     public float carTurnSpeed;
     private Animator playerCarAnimator;
     private int lane;
@@ -29,50 +42,76 @@ public class DrivingGameController : MonoBehaviour
     void Start()
     {
         lane = 2;
-        playerCarAnimator = playerCar.gameObject.GetComponent<Animator>();
+        playerCarAnimator = playerCarScript.gameObject.GetComponentInChildren<Animator>();
+        playerCarScript.dgc = this;
 
         spawnedCars = new List<EnemyCarScript>();
+        leaderboard_object.gameObject.SetActive(false);
 
         StartCoroutine("SpawnLoop");
     }
 
     void Update()
     {
-        targetRoadSpeed = Mathf.Lerp(targetRoadSpeed, roadBaseSpeed + ((level - 1) * 1.5f), roadAcceleration * Time.deltaTime);
-        playerCar.position = new Vector3(Mathf.Lerp(playerCar.position.x, lane, carTurnSpeed * Time.deltaTime), playerCar.position.y, playerCar.position.z);
+        // Update score
+        if (gameOn)
+        {
+            actualScore += Time.deltaTime * scoreModifier;
+            score = Mathf.FloorToInt(actualScore);
+            level = Mathf.CeilToInt(score / difficulty);
+        }
+
+        // Update GUI
+        level_text.text = level.ToString();
+        score_text.text = score.ToString();
+
+        // Lerps
+        if (gameOn)
+        {
+            targetRoadSpeed = Mathf.Lerp(targetRoadSpeed, roadBaseSpeed + ((level - 1) * 1.5f), roadAcceleration * Time.deltaTime);
+            playerCar.position = new Vector3(Mathf.Lerp(playerCar.position.x, lane, carTurnSpeed * Time.deltaTime), playerCar.position.y, playerCar.position.z);
+        }
+        else
+        {
+            targetRoadSpeed = 0.0f;
+        }
 
         // Enemy Cars
-        for (int i = 0; i < spawnedCars.Count; i++)
+        if (gameOn)
         {
-            GameObject go = spawnedCars[i].gameObject;
-            int lane = spawnedCars[i].myLane;
-
-            float speed = enemyCarBaseSpeed;
-            switch (lane)
+            for (int i = 0; i < spawnedCars.Count; i++)
             {
-                case 0:
-                    speed *= 1.5f;
-                    speed += targetRoadSpeed;
-                    break;
-                case 1:
-                    speed *= 2.0f;
-                    speed += targetRoadSpeed;
-                    break;
-                case 2:
-                    speed = targetRoadSpeed - (enemyCarBaseSpeed * 1.5f);
-                    break;
-                case 3:
-                    speed = targetRoadSpeed - enemyCarBaseSpeed;
-                    break;
-            }
+                GameObject go = spawnedCars[i].gameObject;
+                int lane = spawnedCars[i].myLane;
 
-            go.transform.Translate(-go.transform.forward * speed * Time.deltaTime);
+                float speed = enemyCarBaseSpeed;
+                switch (lane)
+                {
+                    case 0:
+                        speed *= 1.5f;
+                        speed += targetRoadSpeed;
+                        break;
+                    case 1:
+                        speed *= 2.0f;
+                        speed += targetRoadSpeed;
+                        break;
+                    case 2:
+                        speed = targetRoadSpeed - (enemyCarBaseSpeed * 1.5f);
+                        break;
+                    case 3:
+                        speed = targetRoadSpeed - enemyCarBaseSpeed;
+                        break;
+                }
+                print(-go.transform.forward * speed * Time.deltaTime);
 
-            if (go.transform.position.z <= -(roadSize * 2.0f))
-            {
-                spawnedCars.Remove(spawnedCars[i]);
-                Destroy(go);
-                i--;
+                go.transform.Translate(-go.transform.forward * speed * Time.deltaTime);
+
+                if (go.transform.position.z <= -(roadSize * 2.0f))
+                {
+                    spawnedCars.Remove(spawnedCars[i]);
+                    Destroy(go);
+                    i--;
+                }
             }
         }
 
@@ -87,30 +126,73 @@ public class DrivingGameController : MonoBehaviour
         }
 
         // Input
-        if (Input.GetKeyDown(KeyCode.A))
+        if (gameOn)
         {
-            lane -= 4;
-            if (lane < -6)
+            if (Input.GetKeyDown(KeyCode.A))
             {
-                lane = -6;
+                lane -= 4;
+                if (lane < -6)
+                {
+                    lane = -6;
+                }
+                else
+                {
+                    playerCarAnimator.SetTrigger("TurnLeft");
+                }
             }
-            else
+            else if (Input.GetKeyDown(KeyCode.D))
             {
-                playerCarAnimator.SetTrigger("TurnLeft");
+                lane += 4;
+                if (lane > 6)
+                {
+                    lane = 6;
+                }
+                else
+                {
+                    playerCarAnimator.SetTrigger("TurnRight");
+                }
             }
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+    }
+
+    public void Crash()
+    {
+        gameOn = false;
+        playerCarAnimator.SetTrigger("Crash");
+        
+    }
+
+    public void ShowLeaderboards()
+    {
+        // Show leaderboards
+        leaderboard_object.gameObject.SetActive(true);
+        level_object.gameObject.SetActive(false);
+        score_object.gameObject.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        leaderboard_object.gameObject.SetActive(false);
+        level_object.gameObject.SetActive(true);
+        score_object.gameObject.SetActive(true);
+        playerCarAnimator.SetTrigger("Restart");
+
+        // Enemy Cars
+        for (int i = 0; i < spawnedCars.Count; i++)
         {
-            lane += 4;
-            if(lane > 6)
-            {
-                lane = 6;
-            }
-            else
-            {
-                playerCarAnimator.SetTrigger("TurnRight");
-            }
+            Destroy(spawnedCars[i].gameObject);
+            spawnedCars.Remove(spawnedCars[i]);
+            i--;
         }
+
+        playerCar.transform.position = new Vector3(2.0f, 0.0f, 0.0f);
+
+        level = 1;
+        score = 0;
+        actualScore = 0.0f;
+        lane = 2;
+
+        gameOn = true;
     }
 
     IEnumerator SpawnLoop()
